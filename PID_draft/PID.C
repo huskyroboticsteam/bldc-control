ffc5bee875654be336e6513bd4eb9c93c8120b30
Becca Caulton <53310516+BeccaCaul@users.noreply.github.com>
2026-01-31 13:26:50 -0800

    PID draft pretty done
    just need to know what data we're getting to know how to adjust it but basic outline is complete

--- PID_draft/PID.C
+++ PID_draft/PID.C
@@ -1,13 +1,19 @@
 //used http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/ as reference
 //imports and stuff
 
+//system assumes consistent intervals (discrete-time PID)
 
-int32_t kPosition = 0, kIntegral = 0, kDerivative = 0;
+//unsure if we need just speed control (using hall sensor) or speed and position (using encoder?)
+int kPosition = 0, kIntegral = 0, kDerivative = 0;
 
-uint32_t maxSpeed = 32768; //taken from last year
+int maxSpeed = 32768; //taken from last year
 
 int integral = 0;
 int lastError = 0;
+
+int maxOut;
+int minOut;
+
 
 void ClearPIDProgress() {
     integral = 0;
@@ -27,21 +33,46 @@ uint8_t PIDIsEnabled() {
 }
 
 void setPosition(){
-
-
+//using PID set position... unsure on where position data is coming from so leave blank for now
 
 }
 
-int32_t positionPID(int32 targetDeg){
+void setK_Values(int32_t kP, int32_t kI, int32_t kD){
+	kPosition = kP;
+	kIntegral = kI;
+	kDerivative = kD;
+}
+
+void setMaxMin(int32_t max, int32_t min){
+	maxOut = max;
+	minOut = min;
+}
+
+int32_t PID(int32_t target){
     if(!PIDIsEnabled()){
         return(0);
     }
 
-    int32 current = GetPositionmDeg();
-    int32 error = targetDeg - current;
+    int current = GetHallSensor(); //from hall sensor/wtv get some sort of data
+    int error = target - current;
 
+    integral = integral + error;
 
+    if(integral > outMax){
+    	integral = outMax;
+    }else if(integral < outMin) {
+    	integral = outMin;
+    }
 
+    int derivative = error - lastError;
+
+    int PWMOut = error*kPosition/10 + integral*kIntegral/10 + derivative*kDerivative/10;
+
+    if(PWMOut > outMax) {
+    	PWMOut = outMax;
+    }else if(Output < outMin) {
+    	PWMOut = outMin;
+    }
 
     lastError = error; //update error
 
